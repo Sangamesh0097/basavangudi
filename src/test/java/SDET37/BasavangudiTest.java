7da6395ba57e03d3c253250dc113a88956bf15d1
Sangamesh0097 <sangamesh0097@gmail.com>
2022-07-25 11:51:31 +0530

    files added to github

---
+++ src/test/java/SDET37/BasavangudiTest.java
@@ -0,0 +1,14 @@
+package SDET37;
+
+public class BasavangudiTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hiiiiii");
+		System.out.println("test yantra!!!");
+		System.out.println("Youuuuu");
+
+	}
+
+}
